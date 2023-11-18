113be3772b95d78f5cc758b25bc7c5de6250433a
Ubuntu <ubuntu@ip-172-31-39-91.ec2.internal>
2023-11-18 17:21:14 +0000

    Added docker-compse.yml file and HTML files...

---
+++ www/html/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$date = date("d-m-Y");
+
+?>
+
+<html>
+<body>
+
+<h2> Welcome in PHP page using docker compose </h2>
+
+<h3> Today date is :<?php echo $date; ?> </h3>
+
+
+<?php 
+echo phpinfo();
+?>
+</body>
+</html>
